d6160112c7ef573c2a4b641ee123446be5d53633
Tim Munroe <munroe.tim@gmail.com>
2018-08-23 18:40:09 -0400

    Aug23 late

--- static/feed.js
+++ static/feed.js
@@ -63,6 +63,7 @@ $(document).ready(function () {
         Sports: {
             Basketball: {
                 NBA: ['Raptors'],
+                NCAA: [""],
             },
             Baseball: {
                 MLB: ['Jays'] // playoffs 'ALCS', 'ALDS', 'NLCS', 'NLDS', 'Wildcard'
